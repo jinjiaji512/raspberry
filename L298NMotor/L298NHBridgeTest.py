2f9d9afeb89805361e8a821e55ee70e41030f09d
jinjiaji <jinjiaji@roobo.com>
2020-01-15 16:27:11 +0800

    update

--- L298NMotor/L298NHBridgeTest.py
+++ L298NMotor/L298NHBridgeTest.py
@@ -6,10 +6,12 @@
 import sys, tty, termios, os
 from L298NHBridge import HBridge
 
+speed = 0
+angle = 0
 speedleft = 0
 speedright = 0
 
-Motors = HBridge(27, 22, 23, 24, 19, 26)
+Motors = HBridge(17, 18, 27, 22, 23, 24)
 # Instructions for when the user has an interface
 print("w/s: direction")
 print("a/d: steering")
@@ -41,113 +43,76 @@ def printscreen():
 	print("q: stops the motors")
 	print("x: exit")
 	print("========== Speed Control ==========")
+	print "speed:  ", speed
+	print "angle:  ", angle
 	print "left motor:  ", speedleft
 	print "right motor: ", speedright
+
+def setMotorSpeed():
+	# generate the left motor speed and right motor speed with the speed and angle
+	speedleft = speed - angle
+	speedright = speed + angle
+	if speedleft < 0:
+		speedleft = 0
+	if speedleft > 1:
+		speedleft = 1
+	if speedright < 0:
+		speedright = 0
+	if speedright > 1:
+		speedright = 1
+	Motors.setMotorLeft(speedleft)
+	Motors.setMotorRight(speedright)
+
 
 while True:
     # Keyboard character retrieval method. This method will save
     # the pressed key into the variable char
 	char = getch()
 
-
-	
 	# The car will drive forward when the "w" key is pressed
 	if(char == "w"):
-	
 		# synchronize after a turning the motor speed
-			
-		# if speedleft > speedright:
-			# speedleft = speedright
-		
-		# if speedright > speedleft:
-			# speedright = speedleft
-				
 		# accelerate the RaPi car
-		speedleft = speedleft + 0.1
-		speedright = speedright + 0.1
+		speed = speed + 0.1
+		if speed > 1:
+			speed = 1
 
-		if speedleft > 1:
-			speedleft = 1
-		if speedright > 1:
-			speedright = 1
-		
-		Motors.setMotorLeft(speedleft)
-		Motors.setMotorRight(speedright)
-		printscreen()
-
-    # The car will reverse when the "s" key is pressed
+	# The car will reverse when the "s" key is pressed
 	if(char == "s"):
-	
-		# synchronize after a turning the motor speed
-			
-		# if speedleft > speedright:
-			# speedleft = speedright
-			
-		# if speedright > speedleft:
-			# speedright = speedleft
-			
-		# slow down the RaPi car
-		speedleft = speedleft - 0.1
-		speedright = speedright - 0.1
+		speed = speed - 0.1
+		if speed < 0:
+			speed = 0
 
-		if speedleft < -1:
-			speedleft = -1
-		if speedright < -1:
-			speedright = -1
-		
-		Motors.setMotorLeft(speedleft)
-		Motors.setMotorRight(speedright)
-		printscreen()
-
-    # Stop the motors
+	# Stop the motors
 	if(char == "q"):
-		speedleft = 0
-		speedright = 0
-		Motors.setMotorLeft(speedleft)
-		Motors.setMotorRight(speedright)
-		printscreen()
+		angle = 0 
+		speed = 0
 
-    # The "d" key will toggle the steering right
+
+	# The "d" key will toggle the steering right
 	if(char == "d"):		
-		#if speedright > speedleft:
-		speedright = speedright - 0.1
-		speedleft = speedleft + 0.1
+		angle = angle - 0.1
+		if angle < -1:
+			angle = -1
 		
-		if speedright < -1:
-			speedright = -1
-		
-		if speedleft > 1:
-			speedleft = 1
-		
-		Motors.setMotorLeft(speedleft)
-		Motors.setMotorRight(speedright)
-		printscreen()
-		
-    # The "a" key will toggle the steering left
+	# The "a" key will toggle the steering left
 	if(char == "a"):
-		#if speedleft > speedright:
-		speedleft = speedleft - 0.1
-		speedright = speedright + 0.1
-			
-		if speedleft < -1:
-			speedleft = -1
-		
-		if speedright > 1:
-			speedright = 1
-		
-		Motors.setMotorLeft(speedleft)
-		Motors.setMotorRight(speedright)
-		printscreen()
-		
+		angle = angle + 0.1
+		if angle > 1:
+			angle = 1
+
 	# The "x" key will break the loop and exit the program
 	if(char == "x"):
-		Motors.setMotorLeft(0)
-		Motors.setMotorRight(0)
+		angle = 0 
+		speed = 0
+		setMotorSpeed()
 		Motors.exit()
 		print("Program Ended")
 		break
 	
-    # The keyboard character variable char has to be set blank. We need
+	# The keyboard character variable char has to be set blank. We need
 	# to set it blank to save the next key pressed by the user
 	char = ""
+	setMotorSpeed()
+	printscreen()
 # End
